b402d64426484807e65131b95b03bbeecd875127
Max Tybar <maxtybar@gmail.com>
2022-01-17 16:17:28 -0600

    SpringBoot Created

---
+++ techbootcamp-MaxTybar/src/main/java/com/perficient/techbootcampMaxTybar/TechbootcampMaxTybarApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampMaxTybar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampMaxTybarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampMaxTybarApplication.class, args);
+	}
+
+}
---
+++ techbootcamp-MaxTybar/src/test/java/com/perficient/techbootcampMaxTybar/TechbootcampMaxTybarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampMaxTybar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbootcampMaxTybarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
